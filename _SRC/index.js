be151fc69591aafab2340afedb12887350d6999d
Anton Burchak <anton.burchak@gmail.com>
2020-03-09 15:26:00 +0200

    update

--- _SRC/index.js
+++ _SRC/index.js
@@ -14,45 +14,45 @@ window.addEventListener('load', () => {
     preload();
     mobClick();
     setTimeout(() => {
-    // first entry animation
-    animText(document.querySelectorAll('.page')[0]);
-
-    
-
-    structAnim();
-    zoomGallery();
-    navigation();
-    
+        // first entry animation
+        animText(document.querySelectorAll('.page')[0]);
 
 
-    let lis = document.querySelectorAll('.text-format .main__section-list li');
-    lis.forEach(element => {
-        element.addEventListener('click', () => {
-            let codetext = element.querySelector('codetext');
-            let codeview = element.querySelector('codeview');
+
+        structAnim();
+        zoomGallery();
+        navigation();
 
 
-            lis.forEach(el => {
-                el.classList.remove('active');
-                el.querySelector('.result').classList.remove('active');
-                element.querySelector('codeview').classList.remove('active');
-                if(el.querySelector('div[data-value="Click me!"]')) {
-                    el.querySelector('div[data-value="Click me!"]').setAttribute('data-value', '');
+
+        let lis = document.querySelectorAll('.text-format .main__section-list li');
+        lis.forEach(element => {
+            element.addEventListener('click', () => {
+                let codetext = element.querySelector('codetext');
+                let codeview = element.querySelector('codeview');
+
+
+                lis.forEach(el => {
+                    el.classList.remove('active');
+                    el.querySelector('.result').classList.remove('active');
+                    element.querySelector('codeview').classList.remove('active');
+                    if (el.querySelector('div[data-value="Click me!"]')) {
+                        el.querySelector('div[data-value="Click me!"]').setAttribute('data-value', '');
+                    }
+                });
+
+
+                if (codetext.textContent.trim() == codetext.getAttribute('data-text')) {
+                    goAnim(codetext);
                 }
-            });
-            
+                element.classList.add('active');
+                element.querySelector('.result').classList.add('active');
 
-            if(codetext.textContent.trim() == codetext.getAttribute('data-text')) {
-                goAnim(codetext);
-            }
-            element.classList.add('active');
-            element.querySelector('.result').classList.add('active');
-            
-            setTimeout(() => codeview.classList.add('active'), codetext.getAttribute('data-text').length * 40);
+                setTimeout(() => codeview.classList.add('active'), codetext.getAttribute('data-text').length * 40);
+            });
         });
-    });
-    }, 0); // 5000
-    
+    }, 4000); // 5000
+
 });
 
 
@@ -79,20 +79,20 @@ const goAnim = (div) => {
     let a = 0;
     let b = setInterval(() => {
         div.textContent += tmp[a];
-        if(a == tmp.length - 1) {
+        if (a == tmp.length - 1) {
             clearInterval(b);
         }
         a++;
     }, 40);
-    
+
 };
 
 const structAnim = () => {
     let list = document.querySelectorAll('.html-structure .main__section-list li'),
         list_i = document.querySelectorAll('.html-structure .sect-wrap .i');
-    
+
     list.forEach((element, index) => {
-        
+
         element.addEventListener('click', (e) => {
             e.preventDefault();
             list[0].querySelector('a').setAttribute('data-value', '');
@@ -103,10 +103,10 @@ const structAnim = () => {
                 }, index * 40);
             });
             list_i[index].classList.add('active');
-            
+
             document.querySelector('.sect-wrap').classList.add('sw-anim');
         });
-       
+
     });
 
     let list_cls = document.querySelectorAll('.html-structure .sect-wrap .i .close');
@@ -114,7 +114,7 @@ const structAnim = () => {
     list_cls.forEach((el) => {
         el.addEventListener('click', () => {
             el.parentElement.classList.remove('active');
-            
+
             list.forEach((item, index) => {
                 setTimeout(() => {
                     item.style.transform = 'translateX(0)';
@@ -133,33 +133,35 @@ const navigation = () => {
     mainContent.forEach(el => el.classList.remove('dont-ready'));
 
     let nav = document.querySelectorAll('nav ul li a'),
-    page = document.querySelectorAll('.page'),
-    liss = document.querySelectorAll('.text-format .main__section-list li codetext');
+        page = document.querySelectorAll('.page'),
+        liss = document.querySelectorAll('.text-format .main__section-list li codetext');
 
     mainContent.forEach(el => el.classList.add('dont-ready'));
 
     nav.forEach((el, index) => {
         el.addEventListener('click', (e) => {
             e.preventDefault();
+            console.log(index);
+            // if (document.documentElement.clientWidth <= 980) page = document.querySelectorAll('.page');
 
             page.forEach(el => {
                 el.querySelector('h1').classList.remove('active');
                 el.querySelector('.main__content').classList.remove('active');
                 el.classList.remove('active');
             });
-            
+
             nav.forEach(item => item.classList.remove('active'));
 
             el.classList.add('active');
-            page[index].classList.add('active');
+            page[index % 4].classList.add('active');
 
             document.body.classList = '';
-        
-            animText(page[index]);
-            
+
+            animText(page[index % 4]);
+
             switch (index) {
                 case 2: {
-                    setTimeout(()=> {
+                    setTimeout(() => {
                         document.body.classList.add('btrfl');
                     }, 2500);
                 }
@@ -168,16 +170,21 @@ const navigation = () => {
                     let chack = [...document.querySelectorAll('table th'), ...document.querySelectorAll('table td')];
                     chack.forEach(el => el.style.opacity = "0");
                 }
+                case 7: {
+                    tableAnimate();
+                    let chack = [...document.querySelectorAll('table th'), ...document.querySelectorAll('table td')];
+                    chack.forEach(el => el.style.opacity = "0");
+                }
                 default: {
                     document.body.classList.remove('btrfl');
                 }
             }
-        
+
             liss.forEach(element => {
                 element.textContent = element.getAttribute('data-text');
-            }); 
-            
-            document.body.classList.add(`style${index + 1}`)
+            });
+
+            document.body.classList.add(`style${index % 4 + 1}`)
         });
     });
 }
@@ -185,10 +192,10 @@ let tmp = 0;
 
 const next = () => {
     let images = document.querySelectorAll('.gallery div img'),
-        current = document.querySelector('.imgParent img');      
+        current = document.querySelector('.imgParent img');
     tmp++;
     if (tmp == images.length) tmp = 0;
-    current.src = images[tmp].src;  
+    current.src = images[tmp].src;
 };
 
 const prev = () => {
@@ -196,7 +203,7 @@ const prev = () => {
         current = document.querySelector('.imgParent img');
     tmp--;
     if (tmp == -1) tmp = images.length - 1;
-    current.src = images[tmp].src; 
+    current.src = images[tmp].src;
 };
 
 const zoomGallery = () => {
@@ -211,11 +218,11 @@ const zoomGallery = () => {
             tmp = index;
         });
     });
-    
+
     parent.addEventListener('click', (e) => {
         let target = e.target || e.srtElement;
 
-        if((target.tagName !== "IMG" && target.className !== "next" && target.className !== "prev") || (target.tagName == "DIV" && target.className == 'close')) {
+        if ((target.tagName !== "IMG" && target.className !== "next" && target.className !== "prev") || (target.tagName == "DIV" && target.className == 'close')) {
             parent.classList.remove('active');
         }
     });
@@ -232,7 +239,7 @@ const tableAnimate = () => {
     let table = document.querySelector('.tables table'),
         tableFirst = document.querySelectorAll('.tables table tr td:first-child');
     let chack = [...document.querySelectorAll('table th'), ...document.querySelectorAll('table td')];
-    
+
     let time = 60;
     let overtime = chack.length * time;
     table.style.opacity = '1';
@@ -243,9 +250,9 @@ const tableAnimate = () => {
                 el.style.opacity = '1';
                 el.style.animation = '.5s tds';
             }, index * time);
-            
+
         });
-    
+
         setTimeout(() => {
             chack.forEach((el, index) => {
                 setTimeout(() => {
@@ -259,11 +266,11 @@ const tableAnimate = () => {
 
 
 const preload = () => {
-    let pB = document.querySelector('.preload'),  
-    pBI = document.querySelector('.preload__inner'),
-    pBIH = document.querySelector('.preload__h'),
-    pBIHA = document.querySelector('.preload__autor');   
-    
+    let pB = document.querySelector('.preload'),
+        pBI = document.querySelector('.preload__inner'),
+        pBIH = document.querySelector('.preload__h'),
+        pBIHA = document.querySelector('.preload__autor');
+
     let words = [
         ['«Packaging is just as important as the product. <br> Sometimes even more important»', 'Jack Trout'],
         ['«Simplicity is a prerequisite for beauty»', 'Lev Tolstoy'],
@@ -272,18 +279,18 @@ const preload = () => {
     ];
 
     let randWord = words[(Math.ceil(getRand(0, words.length - 1)))];
-    
+
     pBIH.innerHTML = randWord[0];
     pBIHA.innerHTML = randWord[1];
 
     pBI.style.opacity = '1';
     setTimeout(() => {
         pB.style.top = '-100%';
-    }, 4500);
+    }, 3500);
 };
 
 function getRand(min, max) {
-  return Math.random() * (max - min) + min;
+    return Math.random() * (max - min) + min;
 }
 
 const mobClick = () => {
@@ -302,9 +309,4 @@ const mobClick = () => {
     });
 
     navigation();
-}
-
-if (document.documentElement.clientWidth <= 980) {
-    let d = document.querySelector('.wrapper .header__list');
-    d.remove();
 }
