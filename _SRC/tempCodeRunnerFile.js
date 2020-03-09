360b1f838dd23bb4ad7d04b5269bebd8b477c8aa
Anton Burchak <anton.burchak@gmail.com>
2020-03-09 15:30:36 +0200

    update

---
+++ _SRC/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+/ 		questionClose = questionForm.querySelector('.close'),
+		// 		workArray = [sendLink, questionClose];
+
+		// 		for(let i = 0; i < workArray.length; i++) {
+		// 			workArray[i].addEventListener('click', function(e) {
+		// 				e.preventDefault();
+		// 				questionForm.classList.toggle('active');
+		// 				bodyBack.classList.toggle('active');
+		// 				document.body.classList.toggle('block');
+		// 			});		
+		// 		}
